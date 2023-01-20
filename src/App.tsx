2fddb0e54434d611e617acc94a0108569cc1d183
Pedro Lucas Rando <pedrolucas363@gmail.com>
2023-01-20 17:00:11 -0300

    first commit of 3 lessons

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/header";
+import { SummaryTable } from "./components/summaryTable";
+import "./styles/global.css";
+
+// import { Habit } from "./components/habit";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
+}
